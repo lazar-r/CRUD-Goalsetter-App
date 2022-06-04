1a3017eab823f8e689978a5e82b4592843c626b6
lazar-r <42014334+lazar-r@users.noreply.github.com>
2022-06-04 15:54:52 +0200

    last push

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,6 @@ const User = require('../models/userModel');
 //@desc    Register new user
 //@route   POST /api/users
 //@access  Public
-
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -16,7 +15,6 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // Check if user exists
-
   const userExists = await User.findOne({ email });
 
   if (userExists) {
@@ -25,12 +23,10 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // Hash password
-
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
   // Create user
-
   const user = await User.create({
     name,
     email,
@@ -53,7 +49,6 @@ const registerUser = asyncHandler(async (req, res) => {
 //@desc    Authenticate a user
 //@route   POST /api/users/login
 //@access  Public
-
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -77,15 +72,8 @@ const loginUser = asyncHandler(async (req, res) => {
 //@desc    Get user data
 //@route   GET /api/users/me
 //@access  Private
-
 const getMe = asyncHandler(async (req, res) => {
-  const { _id, name, email } = await User.findById(req.user.id);
-
-  res.status(200).json({
-    id: _id,
-    name,
-    email,
-  });
+  res.status(200).json(req.user);
 });
 
 // Generate JWT
